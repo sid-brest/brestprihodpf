d39ee23dae70254614c81e3ddb0aeb0714a8e408
sid-brest <isidbrest@gmail.com>
2022-10-13 10:42:28 +0300

    update shell

--- update.sh
+++ update.sh
@@ -1,4 +1,3 @@
 git -C /home/prihodpf/repositories/brestprihodpf pull
 rsync -a --exclude=".*/" /home/prihodpf/repositories/brestprihodpf/ /home/prihodpf/public_html
 chmod 775 /home/prihodpf/public_html
-printf "Folder Updated\n"
