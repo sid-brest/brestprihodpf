bce823e572b545093dd351aa0347df81eea9e536
Dzmitry Struneuski <isidbrest@gmail.com>
2022-10-13 10:25:41 +0300

    Update update.sh
    rsync change

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 git -C /home/prihodpf/repositories/brestprihodpf pull
-rsync -av --exclude=".*/" /home/prihodpf/repositories/brestprihodpf/ /home/prihodpf/public_html
+rsync -a --exclude=".*/" /home/prihodpf/repositories/brestprihodpf/ /home/prihodpf/public_html
 chmod 775 /home/prihodpf/public_html
 printf "Folder Updated\n"
